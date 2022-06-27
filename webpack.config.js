ddc23ad11a791e44361257b083af08f25debb9a6
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2022-06-27 19:16:06 -0300

    Add file loaders and hmr

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,39 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
 
-module.exports = {
-  mode: "development",
-  entry: path.resolve(__dirname, "src", "index.js"),
-  output: {
-    path: path.join(__dirname, "/dist"),
-    filename: "bundle.[contenthash].js",
-  },
-  devtool: "source-map",
-  module: {
-    rules: [
-      { test: /\.(js)$/, exclude: /node_modules/, use: ["babel-loader"] },
-      {
-        test: /\.css$/,
-        use: [{ loader: "style-loader" }, { loader: "css-loader" }],
-      },
+module.exports = ({ mode } = { mode: "production" }) => {
+  console.log(`mode: ${mode}`);
+
+  return {
+    mode,
+    entry: path.resolve(__dirname, "src", "index.js"),
+    output: {
+      path: path.join(__dirname, "/dist"),
+      filename: "bundle.[contenthash].js",
+    },
+    devtool: "source-map",
+    devServer: {
+      open: true,
+      hot: true,
+    },
+    module: {
+      rules: [
+        { test: /\.(js)$/, exclude: /node_modules/, use: ["babel-loader"] },
+        {
+          test: /\.css$/,
+          use: [{ loader: "style-loader" }, { loader: "css-loader" }],
+        },
+        {
+          test: /\.(png|jpe?g|gif)$/i,
+          exclude: /node_modules/,
+          use: ["url-loader", "file-loader"],
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({ template: "./index.html" }),
+      new webpack.HotModuleReplacementPlugin(),
     ],
-  },
-  plugins: [new HtmlWebpackPlugin({ template: "./index.html" })],
+  };
 };
